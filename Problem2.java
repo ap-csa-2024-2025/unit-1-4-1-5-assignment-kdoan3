f3add73ffcf31c5b73610495f1c760c4bf8f7268
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-09-12 11:01:58 -0700

    fdafdsa

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,19 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+    int number = 123;
+    int digit1 = (number % 10);
+    number = number / 10;
 
+    int digit2 = (number % 10);
+    number = number / 10;
+
+    int digit3 = ( number % 10);
+    number = number / 10;
+
+    System.out.println(digit3);
+    System.out.println(digit2);
+    System.out.println(digit1);
+    }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,3 @@
-
+public class Problem3
+{
+    
