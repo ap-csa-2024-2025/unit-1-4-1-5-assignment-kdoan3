b4dd9d5d8ec1da64abd8ab4df0b543b47626ae27
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-09-12 10:47:45 -0700

    hfjdkhajfkdsa

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,12 @@
-
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+   int val = 24;
+   System.out.println(val);
+   val++;
+   System.out.println("val is now: " + val);
+   val--;
+   System.out.println("val is now: " + val);
+  }
+}
